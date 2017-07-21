83c49dac5dc4f9ad72c4b817e45517350920a177
kevalkumar <keval@devrepublic.nl>
2017-07-21 16:17:45 +0530

    test

--- src/test_autoblox/Dev89_Bedrijfsgegevens.java
+++ src/test_autoblox/Dev89_Bedrijfsgegevens.java
@@ -61,8 +61,8 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 
 	public void login_test_suit() throws Exception 
 	{
-		/*try{  */
-/* -------------------------------------------VFS check in company name-------------------------------*/
+		try{  
+
 			login.navigateLogin();
 			logger.info("Enter in website");
 			String loginurl = driver.getCurrentUrl();
@@ -80,24 +80,12 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 			System.out.println(loginid);
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			Thread.sleep(5000);
-			
+			/* -------------------------------------------VFS check in company name-------------------------------*/		
 			mouse_move("id","ctl00_mmMyData_tab", "linkText","Bedrijfsgegevens" );
 			System.out.println("hover done");
 			Thread.sleep(5000);
 			
-			/*
-			//clickElement("xpath","//a[contains(.,'Mijn gegevens')]");
-			clickElement("xpath","//a[contains(@id,'ctl00_mmMyData_lnk')]");
-			//clickElement("id","ctl00_mmMyData_lnk");
-			//hover("id","ctl00_mmMyData_lnk");
-			System.out.println("clicked in menu");
-			Thread.sleep(5000);
 			
-			clickElement("linkText","Bedrijfsgegevens");
-			//clickElement("xpath","//a[contains(.,'Bedrijfsgegevens')]");
-			System.out.println("clicked in company list");
-			Thread.sleep(5000);
-			*/
 			String company_name = driver.findElement(By.id("ctl00_cphContent_tbName")).getAttribute("value");
 			System.out.println(company_name);
 			
@@ -125,7 +113,7 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 					}
 					else 
 					{
-						System.out.println("checkbox is selected");
+						System.out.println("checkbox is already selected");
 		
 					}
 			Thread.sleep(10000);
@@ -149,7 +137,7 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 			System.out.println("VSB radio button selected");
 			Thread.sleep(5000);
 			
-			insertElement("id","ctl00_cphContent_tbVSB","1125AB");
+			insertElement("id","ctl00_cphContent_tbVSB","1995AB");
 			System.out.println("VSB inserted");
 			Thread.sleep(5000);
 			
@@ -157,34 +145,32 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 			System.out.println("click on search");
 			Thread.sleep(10000);
 			
-			/*driver.switchTo().frame("ctl00_cphContent_mctlCompanySelection_pnlControlLoader");
-			System.out.println("move to alert");	
-			Thread.sleep(8000);*/
-			
-			driver.switchTo().alert();
-			System.out.println("move to alert");	
-			Thread.sleep(2000);
-			
+						
 			clickElement("id","ctl00_cphContent_mctlCompanySelection_ucControlLoader_ctl00_lbFilterShowAll");
 			System.out.println("click on ALL in search popup");
 			Thread.sleep(10000);
+			
+			String company_code = driver.findElement(By.xpath(".//*[@id='ctl00_cphContent_mctlCompanySelection_ucControlLoader_ctl00_gvList']/tbody/tr[2]/td[4]")).getText();
+			System.out.println("company code:-> " + company_code);
 			
 			clickElement("id","ctl00_cphContent_mctlCompanySelection_ucControlLoader_ctl00_gvList_ctl03_ibSelect");
 			System.out.println("select company");
 			Thread.sleep(10000);
 			
-			driver.switchTo().defaultContent();
-			Thread.sleep(8000);
+			
 			
 			clickElement("id","ctl00_cphContent_bNext");
 			System.out.println("Click on Next");
 			Thread.sleep(8000);
 			
-			dropdownByValue("id","ctl00_cphContent_ddStatus","497");
+			String step2 = driver.findElement(By.id("ctl00_cphContent_lbSectionSpecification")).getText();
+			System.out.println(step2);
+			
+			dropdownByValue("id","ctl00_cphContent_ddlBrand","497");
 			System.out.println("select Merk value");
 			Thread.sleep(5000);
 			
-			dropdownByValue("id","ctl00_cphContent_ddlModel","2664");
+			dropdownByValue("id","ctl00_cphContent_ddlModel","2618");
 			System.out.println("select Model value");
 			Thread.sleep(5000);
 			
@@ -192,17 +178,111 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 			System.out.println("value inserted");
 			Thread.sleep(5000);
 
-//		}
+			//date picker
+			clickElement("id","ctl00_cphContent_dsDateFirstRegINT_imgDateS");
+			System.out.println("Click on date picker");
+			Thread.sleep(4000);
+			dropdownByValue("id","ddMonth","4");
+			System.out.println("select Month");
+			Thread.sleep(2000);
+			dropdownByValue("id","ddYear","2016");
+			System.out.println("select year");
+			Thread.sleep(2000);
+			
+			clickElement("xpath",".//*[@id='ctl00_cphContent_dsDateFirstRegINT_pnlPicker']/div[5]/div[4]");
+			System.out.println("select date 12 ");
+			Thread.sleep(5000);
+			System.out.println("date picker closed ");
+			
+			insertElement("id","ctl00_cphContent_tbDashboardMileage","2004");
+			System.out.println("values inserted");
+			Thread.sleep(2000);
+			clickElement("id","ctl00_cphContent_rblDashboardMileageUnit_1");
+			System.out.println("clicked in mijl");
+			Thread.sleep(2000);
+			
+			clickElement("id","ctl00_cphContent_rbBTWYes");
+			System.out.println("clicked in Fiscaal");
+			Thread.sleep(2000);
+			
+			
+			clickElement("id","ctl00_cphContent_bNext");
+			System.out.println("clicked on Next");
+			Thread.sleep(2000);
+			
+			//step-3
+			
+			insertElement("id","ctl00_cphContent_ucProtocolData_0_ucLocation_tbStreetName","test");
+			System.out.println("insert streetname");
+			Thread.sleep(2000);
+			
+			insertElement("id","ctl00_cphContent_ucProtocolData_0_ucLocation_tbPostcode","12452");
+			System.out.println("insert postalcode");
+			Thread.sleep(2000);
+			
+			insertElement("id","ctl00_cphContent_ucProtocolData_0_ucLocation_tbCity","testas");
+			System.out.println("insert city");
+			Thread.sleep(2000);
+			
+			clickElement("id","ctl00_cphContent_bNext");
+			System.out.println("clicked on Finish");
+			Thread.sleep(10000);
+			
+			
+			String finishinfo = driver.findElement(By.id("ctl00_cphContent_lbFinishInfo")).getText();
+			System.out.println(finishinfo);
+			
+			String str = finishinfo;
+			String replaceString = str.replace("="," ");
+			String replaceString1 = replaceString.replace(","," ");
+			String delimiter = " ";
 
-		/*catch(Exception e){
+			String[] temp;
+			temp = replaceString1.split(delimiter);
+			/*for(int i =0; i < temp.length ; i++)
+			{
+				System.out.println(temp[i]);
+				
+			
+			}			
+			System.out.println("***********************");
+			System.out.println("temp[7]:-> " + temp[7]);
+			
+			System.out.println("temp[14]:-> " + temp[14]);*/
+			String Kenteken= temp[7];
+			System.out.println(Kenteken);
+			
+			
+			//open my cars page
+			
+			mouse_move("id","ctl00_mmMyCars_lnk", "linkText","Mijn auto's (beta)" );
+			System.out.println("open my cars page");
+			Thread.sleep(10000);
+			
+			//search Kenteken
+			
+			
+			insertElement("id","ctl00_cphContent_tbSearchTXT",Kenteken);
+			Thread.sleep(2000);
+			clickElement("id","ctl00_cphContent_ibSearchA");
+			Thread.sleep(2000);
+			
+			String Kenteken_details = driver.findElement(By.xpath(".//*[@id='ctl00_cphContent_gvList']/tbody/tr[2]")).getText();
+			System.out.println(Kenteken_details);
+			
+			
+			
+}
 
-			//Thread.sleep(15000);
+	catch(Exception e){
+
+			Thread.sleep(15000);
 			System.out.println("Unexpected Error");
 			logger.info("Unexpected Error");
-			//driver.quit();
-			//System.out.println("quit done");
+			driver.quit();
+			System.out.println("quit done");
 
-		}*/
+		}
 
 
 
@@ -218,15 +298,15 @@ public class Dev89_Bedrijfsgegevens extends keywords {
 	@AfterClass
 	public  static void tearDown() throws Exception {
 
-		/*Thread.sleep(15000);
+		Thread.sleep(15000);
 		login.logout();
 		System.out.println("logout successfull");
 		logger.info("logout successfully");
 		Thread.sleep(5000);
 		driver.quit();
 		Thread.sleep(5000);
-		System.out.println("quit done");*/
-		System.out.println("ERROR");
+		System.out.println("quit done");
+		System.out.println("Done");
 
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
